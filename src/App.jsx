f004e8bdb6dd3b315f27fbc8e9e937fd7542044e
Tan Zhi Rong <jaysongt3233@gmail.com>
2024-10-14 13:30:31 +0800

    Folder restructuring

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './HomePage';
+import HomePage from '../src/pages/home/HomePage';
 
 function App() {
   return (
