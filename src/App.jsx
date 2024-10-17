534a6297deff0a2824bc0cae0bb4d78fe6ac8e96
Tan Zhi Rong <jaysongt3233@gmail.com>
2024-10-18 01:32:04 +0800

    Revert "Merge remote-tracking branch 'origin/JE-ContactUs' into tan-zhi-rong"
    This reverts commit df0003da90a2b856646fb5c64e2c68cde715eb8e, reversing
    changes made to a453af03838a7edb01af1143ed4577d5720c2742.

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ const Register = lazy(() => import("./pages/auth/register"));
 const ShopProduct = lazy(() => import("./pages/shop/shop-product"));
 const Cart = lazy(() => import("./pages/shop/cart/cart"));
 const Checkout = lazy(() => import("./pages/shop/checkout/checkout"));
-const Contact = lazy(() => import("./pages/contact/index"));
 
 function App() {
   return (
@@ -34,9 +33,6 @@ function App() {
       </Routes>
       <Routes>
         <Route path="/product" element={<ShopProduct />} />
-      </Routes>
-      <Routes>
-        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
   );
