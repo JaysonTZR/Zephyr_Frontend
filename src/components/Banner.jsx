a3576ffcf1f2bd5cb552c98640611b4ead0bd580
Tan Zhi Rong <jaysongt3233@gmail.com>
2024-10-23 16:35:40 +0800

    commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import BannerImage from "../assets/images/banner-background.jpg";
 
 const Banner = ({ bannerText }) => {
     return (
-        <div className="flex justify-center mb-16" style={{ backgroundColor: '#f3f2ee' }}>
-            <div className="flex items-center h-40">
-                <span className="text-3xl font-bold">{bannerText}</span>
+        <div className="flex justify-center mb-16">
+            <img
+                src={BannerImage}
+                alt="Banner background"
+                className="absolute object-cover h-60 w-full"
+            />
+            <div className="flex items-center h-60 z-10">
+                <span className="text-4xl font-bold text-white tracking-wide">{bannerText}</span>
             </div>
       </div>
     );
