c6b99bc1a9ed533e8b39de2856f4f0c51304cd7a
Tan Zhi Rong <jaysongt3233@gmail.com>
2024-12-13 17:56:44 +0800

    Add .env

--- src/constant/constants.js
+++ src/constant/constants.js
@@ -1,9 +1,9 @@
 export const companyName = "ZEPHYR.";
-export const apiUrl = "http://localhost:5000/api/";
+export const apiUrl = "http://98.85.108.221/api/";
 
 // AWS
-export const accessKeyId = 'ASIAU3A46UUIFQAIJRMB';
-export const secretAccessKey = '6hD0Ck8ZVu1kQ2QTfdzp3Ou6nanadKlQ84rWbqyP';
-export const sessionToken = 'IQoJb3JpZ2luX2VjEMz//////////wEaCXVzLXdlc3QtMiJHMEUCIQDLNUts4VMI8yiIUZPsdrJYE1etgYRZBLksKs+mwubyeQIgMnJP2ggRQfh4dJOHbY3+WKWrE6aAMQhGNQcVzAaE7qwquwIIhf//////////ARAAGgwzMzI5MjA2OTQwMzIiDNEDklfyO1Kl9LNf+CqPAky0nOIQxwsJ2vwUSVKOqDo0JA6CWhZyWC+2rREk4lw3Wji8ly8MYOBPTB1M3fNsHVa2KbP1g4f7HmQWO3znwQSKcyTOrEzLLfbWmu8gQ0UKbhfu2e+q1HaRen81djPqqHOCcoFUNFgWDC57YA9ZWOT5k4rpCa8c6pHi78ch9Roy+s+dNOzjCP8ys24I+nfvpO6we8gKqUuw47eBy4mtdwiDOj6g39HccfLVMn03e1llmd9NxGmC8e6V0P7tI/opDYj7JvK+ZgypL/GIBzubC3oLgXwkqiiAQI5PrQTgvEUazKj0KKw3GYTSRp9s2qdHrrGYjfJ4ah6iQmqMk10ZyiBFfkh0F0+/i19aDKoKjjswo/3eugY6nQHALWP6pZEzPsNwWSI2SsdESFyl0mdZms6h+FNAXHnOHXDWB19sk8QxrJaP8+IQg3b4wgX0MIN342vdjtu12chrufRlTaHTGae5hnH320wcOym+sBJWOA8QRBB9YSW0SEm2IEv0OO9t7ddUEXr/+J5Dxu7gZAjLQ+pE+wtTNX0JwTS9X6LP8rdMZJrlp+buI4Z/8V6qjIrVM5kEqAHE';
-export const region = 'us-east-1';
-export const bucket = 'zephyr-bucket-demo';
+export const accessKeyId = process.env.REACT_APP_ACCESS_KEY_ID;
+export const secretAccessKey = process.env.REACT_APP_SECRET_ACCESS_KEY;
+export const sessionToken = process.env.REACT_APP_SESSION_TOKEN;
+export const region = process.env.REACT_APP_REGION;
+export const bucket = process.env.REACT_APP_BUCKET;
